f611a0ef6fd5d2e66c33e8a0fdb994cc5e7b5662
ramin rowshan <raminrowshan153@gmail.com>
2021-06-13 21:38:15 +0430

    Setup start project

---
+++ src/main/java/com/project/linkedindatabase/LinkedinDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.project.linkedindatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedinDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedinDatabaseApplication.class, args);
+	}
+
+}
