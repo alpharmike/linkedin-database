c76683ed41fb4521eb94004dd18e49178b76c086
Erfan Moeini <ec.moeini01@gmail.com>
2021-06-18 18:00:29 +0430

    Add types table creation to init method in init api

--- src/main/java/com/project/linkedindatabase/rest/InitializerController.java
+++ src/main/java/com/project/linkedindatabase/rest/InitializerController.java
@@ -1,7 +1,8 @@
 package com.project.linkedindatabase.rest;
 
 import com.project.linkedindatabase.service.BaseTypeService;
-import com.project.linkedindatabase.service.typesmap.AccomplishmentTypeServiceMap;
+import com.project.linkedindatabase.service.typesmap.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,14 +13,54 @@ import java.sql.SQLException;
 @RequestMapping("api/")
 public class InitializerController {
     private final AccomplishmentTypeServiceMap accomplishmentTypeService;
+    private final BackgroundTypeServiceMap backgroundTypeService;
+    private final ConnectTypeServiceMap connectTypeService;
+    private final FormerNameVisibilityTypeServiceMap formerNameVisibilityTypeService;
+    private final IndustryServiceMap industryService;
+    private final LanguageLevelServiceMap languageLevelService;
+    private final NotificationTypeServiceMap notificationTypeService;
+    private final PhoneTypeServiceMap phoneTypeService;
+    private final RelationKnowledgeServiceMap relationKnowledgeService;
+    private final ShowPostTypeServiceMap showPostTypeService;
+    private final SkillLevelServiceMap skillLevelService;
 
-    public InitializerController() throws SQLException {
-        this.accomplishmentTypeService = new AccomplishmentTypeServiceMap();
+    @Autowired
+    public InitializerController(AccomplishmentTypeServiceMap accomplishmentTypeService,
+                                 BackgroundTypeServiceMap backgroundTypeService,
+                                 ConnectTypeServiceMap connectTypeService,
+                                 FormerNameVisibilityTypeServiceMap formerNameVisibilityTypeService,
+                                 IndustryServiceMap industryService, LanguageLevelServiceMap languageLevelService,
+                                 NotificationTypeServiceMap notificationTypeService,
+                                 PhoneTypeServiceMap phoneTypeService,
+                                 RelationKnowledgeServiceMap relationKnowledgeService,
+                                 ShowPostTypeServiceMap showPostTypeService, SkillLevelServiceMap skillLevelService) throws SQLException {
+
+        this.accomplishmentTypeService = accomplishmentTypeService;
+        this.backgroundTypeService = backgroundTypeService;
+        this.connectTypeService = connectTypeService;
+        this.formerNameVisibilityTypeService = formerNameVisibilityTypeService;
+        this.industryService = industryService;
+        this.languageLevelService = languageLevelService;
+        this.notificationTypeService = notificationTypeService;
+        this.phoneTypeService = phoneTypeService;
+        this.relationKnowledgeService = relationKnowledgeService;
+        this.showPostTypeService = showPostTypeService;
+        this.skillLevelService = skillLevelService;
     }
 
     @GetMapping("init")
     public void initialize() throws SQLException {
         this.accomplishmentTypeService.createTable();
+        this.backgroundTypeService.createTable();
+        this.connectTypeService.createTable();
+        this.formerNameVisibilityTypeService.createTable();
+        this.industryService.createTable();
+        this.languageLevelService.createTable();
+        this.notificationTypeService.createTable();
+        this.phoneTypeService.createTable();
+        this.relationKnowledgeService.createTable();
+        this.showPostTypeService.createTable();
+        this.skillLevelService.createTable();
     }
 
 
