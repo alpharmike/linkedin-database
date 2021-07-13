f0266688d76754d2e3bde0769b205aceff139953
Alireza Davoodi <alirezadavoodi1378@gmail.com>
2021-07-14 01:54:22 +0430

    Added Retrieving Request(s), Updating Request, Sending Request, Checking Existence.
    Modified ConnectTypeRepository convertSql() had some error which got fixed, also getByType() method added.

--- src/main/java/com/project/linkedindatabase/repository/types/ConnectTypeRepository.java
+++ src/main/java/com/project/linkedindatabase/repository/types/ConnectTypeRepository.java
@@ -1,10 +1,12 @@
 package com.project.linkedindatabase.repository.types;
 
+import com.project.linkedindatabase.domain.Connect;
 import com.project.linkedindatabase.domain.Type.BackgroundType;
 import com.project.linkedindatabase.domain.Type.ConnectType;
 import com.project.linkedindatabase.repository.BaseTypeRepository;
 import com.project.linkedindatabase.service.types.ConnectTypeService;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -18,13 +20,30 @@ public class ConnectTypeRepository extends BaseTypeRepository<ConnectType> {
         return super.findByName(name);
     }
 
+    public ConnectType getByType(String type) throws SQLException {
+        PreparedStatement retrievePs = this.conn.prepareStatement("SELECT * FROM "+this.tableName+" WHERE name=?");
+        retrievePs.setString(1, type);
+        ResultSet resultSet = retrievePs.executeQuery();
+        return this.convertSql(resultSet);
+    }
+
+
+
     @Override
     public ConnectType convertSql(ResultSet resultSet) throws SQLException {
-        String name = resultSet.getString(NAME);
-        Long id = resultSet.getLong(ID);
-        var type = new ConnectType();
-        type.setId(id);
-        type.setName(name);
+        ConnectType type = new ConnectType();
+        if(!resultSet.next()){
+            return null;
+        }
+        try{
+            String name = resultSet.getString(NAME);
+            Long id = resultSet.getLong(ID);
+            type.setId(id);
+            type.setName(name);
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
 
         return type;
     }
